856ac89b7b18c5c05f338397539f259521f9e447
indritalls <95203855+indritalls@users.noreply.github.com>
2021-12-01 17:40:09 +0700

    Update app.py

--- app.py
+++ app.py
@@ -29,9 +29,9 @@ Jawab = ["facebook","path", "twitter", "instagram", "pinterest", "linkedin", "pl
 app = Flask(__name__)
 
 # Channel Access Token
-line_bot_api = LineBotApi('GUw/Sdi+cGaiCtLq8ZdrYaOMTLk4K1Tc6R0DRiEH/vBqQljRKQ3pJq+oN1sYKNSeSqIPRMJS/H1sBEBIJuxLat77L1VtPgRBnssLOC48ICWaIEk1f9oixGL+aeqgL7mEe6hjk7HUwjSAqjdLBeQA0gdB04t89/1O/w1cDnyilFU=')
+line_bot_api = LineBotApi('k6Y9+0gsK+ycPoenaZtDwyVeW/hzm8Y8XYmXySGv6yVOkRMhcTgn5H3QjvEY/pnFHpayH9oBeJas4QZa4YswY5UxKR4P4t1vVi955fDVpXej7GL0E2p7F19fHznOQeka/W/m6e5j/2VogY0+d5e2HQdB04t89/1O/w1cDnyilFU=')
 # Channel Secret
-handler = WebhookHandler('923c01d65919b7ae347b0749bde3bb6d')
+handler = WebhookHandler('3c71b07a2d7861abf6a78d8845338baf')
 
 # 監聽所有來自 /callback 的 Post Request
 @app.route("/callback", methods=['POST'])
