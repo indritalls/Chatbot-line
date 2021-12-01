5ce31a2b2b4fe695fa8b05f21a62589fc91659d2
indritalls <95203855+indritalls@users.noreply.github.com>
2021-12-01 16:45:14 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
-import operator
-import random
+import sys
+from random import randint
 
 from flask import Flask, request, abort
 
@@ -20,13 +20,30 @@ negara = parsed["name"]
 positif = parsed["positif"]
 sembuh = parsed["sembuh"]
 meninggal = parsed["meninggal"]
+
+count = 0
+ 
+Soal = ["Social Media buatan Mark zuck...? ",
+     "Social Media yang eksis dengan awake sleep? ",
+     "Microblogging yang gambar burung apa hayo? ",
+     "Social Media yang populer dengan photo?",
+     "Social Media yang logonya hampir sama dengan Path  ",
+     "Social Media buat pekerja itu namanya: ",
+     "Planet Python di Indonesia itu hanya: "]
+Jawab = ["facebook",
+     "path",
+     "twitter",
+     "instagram",
+     "pinterest",
+     "linkedin",
+     "planpin"]
 ########################################
 app = Flask(__name__)
 
 # Channel Access Token
-line_bot_api = LineBotApi('k6Y9+0gsK+ycPoenaZtDwyVeW/hzm8Y8XYmXySGv6yVOkRMhcTgn5H3QjvEY/pnFHpayH9oBeJas4QZa4YswY5UxKR4P4t1vVi955fDVpXej7GL0E2p7F19fHznOQeka/W/m6e5j/2VogY0+d5e2HQdB04t89/1O/w1cDnyilFU=')
+line_bot_api = LineBotApi('GUw/Sdi+cGaiCtLq8ZdrYaOMTLk4K1Tc6R0DRiEH/vBqQljRKQ3pJq+oN1sYKNSeSqIPRMJS/H1sBEBIJuxLat77L1VtPgRBnssLOC48ICWaIEk1f9oixGL+aeqgL7mEe6hjk7HUwjSAqjdLBeQA0gdB04t89/1O/w1cDnyilFU=')
 # Channel Secret
-handler = WebhookHandler('3c71b07a2d7861abf6a78d8845338baf')
+handler = WebhookHandler('923c01d65919b7ae347b0749bde3bb6d')
 
 # 監聽所有來自 /callback 的 Post Request
 @app.route("/callback", methods=['POST'])
@@ -43,42 +60,32 @@ def callback():
         abort(400)
     return 'OK'
 
-score = 0
-opslist = {operator.add: "+", operator.sub: "-", operator.mul: "x"} #All operators that can be chosen
-num1,num2 = random.randint(1,10), random.randint(1,10)        #Two Random Numbers          
-ops = random.choice(list(opslist.keys()))        # random operators from oplist keys                        
-ActualAnswer = (ops(num1,num2))                #Answer for my quiz                                
 
+# 處理訊息
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
+    count = 0
     msg_from_user = event.message.text
-    if msg_from_user == 'Kuis':
-            message = TextSendMessage(num1,opslist[ops],num2)
-    	    line_bot_api.reply_message(event.reply_token, message)
-             
-            userAns = (int(input("Enter answer:")))
-            if userAns == ActualAnswer:        
-                message = TextSendMessage("benar")
-    	        line_bot_api.reply_message(event.reply_token, message)
-                return 1
-            else:
-                message = TextSendMessage("salah")
-    	        line_bot_api.reply_message(event.reply_token, message)
-                nilai = score - 0
-            return 0
+    if msg_from_user == 'Data-covid':
+    	message = TextSendMessage("Data COVID-19 " + negara + "\nPositif: " + positif + "\nSembuh: " + sembuh + "\nMeninggal: " + meninggal)
+    	line_bot_api.reply_message(event.reply_token, message)
     
-    totalScore = 0
-    for i in range (10):
-        totalScore += handle_message()
+    i = randint(0, 7)
+    answer = input("Pertanyaan 1 : " + Soal[i])
+    if answer.lower() == Jawab[i]: 
+        count = count + 1
+        message = TextSendMessage("benar"+"\nKamu benar"+count)
+    	line_bot_api.reply_message(event.reply_token, message)  
+    else:
+        message = TextSendMessage("salah")
+    	line_bot_api.reply_message(event.reply_token, message)
 
-    message = TextSendMessage("Kuis telah selesai")
-    line_bot_api.reply_message(event.reply_token, message)
-
-    message = TextSendMessage("Today you achieved a score of" ,totalScore,"out of 10")
-    line_bot_api.reply_message(event.reply_token, message)  
-
-import os
-if __name__ == "__main__":
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
-
+    i = randint(0, 7) 
+    answer = input("\nPertanyaan 2 : " + Soal[i])
+    if answer.lower() == Jawab[i]:
+        count = count + 1
+        message = TextSendMessage("benar"+"\nKamu benar"+count)
+    	line_bot_api.reply_message(event.reply_token, message)
+    else:
+        message = TextSendMessage("salah")
+    	line_bot_api.reply_message(event.reply_token, message)
