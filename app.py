a8a7e7c7a971aa2bc7204ea366f86cdad69deb09
indritalls <95203855+indritalls@users.noreply.github.com>
2021-12-01 10:35:55 +0700

    Update app.py

--- app.py
+++ app.py
@@ -40,12 +40,181 @@ def callback():
         abort(400)
     return 'OK'
 
+from __future__ import unicode_literals, absolute_import
+
+import unittest
+
+from linebot.models import (
+    TemplateSendMessage,
+    CarouselTemplate,
+    CarouselColumn,
+    ImageCarouselTemplate,
+    ImageCarouselColumn,
+    ConfirmTemplate,
+    ButtonsTemplate,
+    PostbackAction,
+    URIAction,
+    MessageAction,
+)
+
+from tests.models.serialize_test_case import SerializeTestCase
+
+
+class TestTemplate(SerializeTestCase):
+    def test_template(self):
+        arg = {
+            'type': 'template',
+            'alt_text': 'This is a buttons template',
+            'template':
+                ButtonsTemplate(
+                    thumbnail_image_url='https://img.tek.id/img/content/2019/07/04/17857/line-resmi-hadirkan-gim-hello-bt21-vZhHl6aAVg.jpg',
+                    image_aspect_ratio='rectangle',
+                    image_size='cover',
+                    image_background_color='#FFFFFF',
+                    title='Menu',
+                    text='Please select',
+                    default_action=URIAction(label='View detail',
+                                             uri='https://img.tek.id/img/content/2019/07/04/17857/line-resmi-hadirkan-gim-hello-bt21-vZhHl6aAVg.jpg'),
+                    actions=[
+                        PostbackAction(label='Buy', data='action=buy&itemid=155'),
+                        URIAction(label='View detail', uri='https://img.tek.id/img/content/2019/07/04/17857/line-resmi-hadirkan-gim-hello-bt21-vZhHl6aAVg.jpg'),
+                    ]
+                )
+        }
+        self.assertEqual(
+            self.serialize_as_dict(arg, type=self.TEMPLATE),
+            TemplateSendMessage(**arg).as_json_dict()
+        )
+
+    def test_button_template(self):
+        arg = {
+            'type': 'buttons',
+            'thumbnail_image_url': 'https://img.tek.id/img/content/2019/07/04/17857/line-resmi-hadirkan-gim-hello-bt21-vZhHl6aAVg.jpg',
+            'image_aspect_ratio': 'rectangle',
+            'image_size': 'cover',
+            'image_background_color': '#FFFFFF',
+            'title': 'Menu',
+            'text': 'Please select',
+            'default_action': URIAction(label='View detail',
+                                        uri='https://img.tek.id/img/content/2019/07/04/17857/line-resmi-hadirkan-gim-hello-bt21-vZhHl6aAVg.jpg'),
+            'actions': [
+                PostbackAction(label='Buy', data='action=buy&itemid=155'),
+                URIAction(label='View detail', uri='https://img.tek.id/img/content/2019/07/04/17857/line-resmi-hadirkan-gim-hello-bt21-vZhHl6aAVg.jpg'),
+            ]
+        }
+        self.assertEqual(
+            self.serialize_as_dict(arg, type=self.BUTTONS),
+            ButtonsTemplate(**arg).as_json_dict()
+        )
+
+    def test_confirm_template(self):
+        arg = {
+            'type': 'confirm',
+            'text': 'Are you sure?',
+            'actions': [
+                MessageAction(label='Yes', text='yes...'),
+                MessageAction(label='No', text='no...'),
+            ]
+        }
+        self.assertEqual(
+            self.serialize_as_dict(arg, type=self.CONFIRM),
+            ConfirmTemplate(**arg).as_json_dict()
+        )
+
+    def test_carousel_template(self):
+        arg = {
+            'image_aspect_ratio': 'rectangle',
+            'image_size': 'cover',
+            'columns': [
+                CarouselColumn(
+                    thumbnail_image_url='https://img.tek.id/img/content/2019/07/04/17857/line-resmi-hadirkan-gim-hello-bt21-vZhHl6aAVg.jpg',
+                    image_background_color='#FFFFFF',
+                    title='this is menu',
+                    text='description',
+                    default_action=URIAction(label='View detail',
+                                             uri='https://img.tek.id/img/content/2019/07/04/17857/line-resmi-hadirkan-gim-hello-bt21-vZhHl6aAVg.jpg'),
+                    actions=[
+                        PostbackAction(label='Buy', data='action=buy&itemid=155'),
+                        URIAction(label='View detail', uri='https://img.tek.id/img/content/2019/07/04/17857/line-resmi-hadirkan-gim-hello-bt21-vZhHl6aAVg.jpg'),
+                    ]
+                ),
+                CarouselColumn(
+                    thumbnail_image_url='https://img.tek.id/img/content/2019/07/04/17857/line-resmi-hadirkan-gim-hello-bt21-vZhHl6aAVg.jpg',
+                    image_background_color='#000000',
+                    title='this is menu',
+                    text='description',
+                    default_action=URIAction(label='View detail',
+                                             uri='https://img.tek.id/img/content/2019/07/04/17857/line-resmi-hadirkan-gim-hello-bt21-vZhHl6aAVg.jpg'),
+                    actions=[
+                        PostbackAction(label='Buy', data='action=buy&itemid=555'),
+                        URIAction(label='View detail', uri='https://img.tek.id/img/content/2019/07/04/17857/line-resmi-hadirkan-gim-hello-bt21-vZhHl6aAVg.jpg'),
+                    ]
+                )
+            ]
+        }
+        self.assertEqual(
+            self.serialize_as_dict(arg, type=self.CAROUSEL),
+            CarouselTemplate(**arg).as_json_dict()
+        )
+
+    def test_carousel_column(self):
+        arg = {
+            'thumbnail_image_url': 'https://img.tek.id/img/content/2019/07/04/17857/line-resmi-hadirkan-gim-hello-bt21-vZhHl6aAVg.jpg',
+            'image_background_color': '#FFFFFF',
+            'title': 'this is menu',
+            'text': 'description',
+            'default_action': URIAction(label='View detail',
+                                        uri='https://img.tek.id/img/content/2019/07/04/17857/line-resmi-hadirkan-gim-hello-bt21-vZhHl6aAVg.jpg'),
+            'actions': [
+                PostbackAction(label='Buy', data='action=buy&itemid=155'),
+                URIAction(label='View detail', uri='https://img.tek.id/img/content/2019/07/04/17857/line-resmi-hadirkan-gim-hello-bt21-vZhHl6aAVg.jpg'),
+            ]
+        }
+        self.assertEqual(
+            self.serialize_as_dict(arg),
+            CarouselColumn(**arg).as_json_dict()
+        )
+
+    def test_image_carousel_template(self):
+        arg = {
+            'columns': [
+                ImageCarouselColumn(
+                    image_url='https://img.tek.id/img/content/2019/07/04/17857/line-resmi-hadirkan-gim-hello-bt21-vZhHl6aAVg.jpg',
+                    action=PostbackAction(label='Buy', data='action=buy&itemid=555')
+                ),
+                ImageCarouselColumn(
+                    image_url='https://img.tek.id/img/content/2019/07/04/17857/line-resmi-hadirkan-gim-hello-bt21-vZhHl6aAVg.jpg',
+                    action=MessageAction(label='Yes', text='yes')
+                ),
+            ]
+        }
+        self.assertEqual(
+            self.serialize_as_dict(arg, type=self.IMAGE_CAROUSEL),
+            ImageCarouselTemplate(**arg).as_json_dict()
+        )
+
+    def test_image_carousel_column(self):
+        arg = {
+            'image_url': 'https://img.tek.id/img/content/2019/07/04/17857/line-resmi-hadirkan-gim-hello-bt21-vZhHl6aAVg.jpg',
+            'action': PostbackAction(label='Buy', data='action=buy&itemid=555')
+        }
+        self.assertEqual(
+            self.serialize_as_dict(arg),
+            ImageCarouselColumn(**arg).as_json_dict()
+        )
+
+x = TestTemplate()
+hasil = TestTemplate.test_template()
+
+if __name__ == '__main__':
+    unittest.main()
+
 # 處理訊息
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     msg_from_user = event.message.text
     if msg_from_user == 'Data-covid':
-    	message = TextSendMessage("Data COVID-19 " + negara + "\nPositif: " + positif + "\nSembuh: " + sembuh + "\nMeninggal: " + meninggal)
+    	message = TextSendMessage(hasil)
     	line_bot_api.reply_message(event.reply_token, message)
 
 import os
