6861ea35fa1934eae9ca55388b807ba3541ecaef
indritalls <95203855+indritalls@users.noreply.github.com>
2021-12-03 10:42:36 +0700

    Update app.py

--- app.py
+++ app.py
@@ -17,6 +17,20 @@ line_bot_api = LineBotApi('k6Y9+0gsK+ycPoenaZtDwyVeW/hzm8Y8XYmXySGv6yVOkRMhcTgn5
 # Channel Secret
 handler = WebhookHandler('3c71b07a2d7861abf6a78d8845338baf')
 
+@app.route("/callback", methods=['POST'])
+def callback():
+    # get X-Line-Signature header value
+    signature = request.headers['X-Line-Signature']
+    # get request body as text
+    body = request.get_data(as_text=True)
+    app.logger.info("Request body: " + body)
+    # handle webhook body
+    try:
+        handler.handle(body, signature)
+    except InvalidSignatureError:
+        abort(400)
+    return 'OK'
+
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     msg_from_user = event.message.text
