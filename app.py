a4f8eb3de830adf57979ddc38d449c9527cba0f2
indritalls <95203855+indritalls@users.noreply.github.com>
2021-12-06 13:16:57 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,54 +1,41 @@
 from flask import Flask, request, abort
-
 from linebot import (
     LineBotApi, WebhookHandler
 )
 from linebot.exceptions import (
     InvalidSignatureError
 )
-from linebot.models import *
-#tambahkan ini#########################
-import requests
-import json
-url = "https://api.kawalcorona.com/indonesia/"
-response = requests.get(url)
-parsed = response.json()[0]
-negara = parsed["name"]
-positif = parsed["positif"]
-sembuh = parsed["sembuh"]
-meninggal = parsed["meninggal"]
-########################################
+from linebot.models import (
+    MessageEvent, TextMessage, TextSendMessage, ImageMessage, ImageSendMessage
+)
+
 app = Flask(__name__)
 
-# Channel Access Token
-line_bot_api = LineBotApi('k6Y9+0gsK+ycPoenaZtDwyVeW/hzm8Y8XYmXySGv6yVOkRMhcTgn5H3QjvEY/pnFHpayH9oBeJas4QZa4YswY5UxKR4P4t1vVi955fDVpXej7GL0E2p7F19fHznOQeka/W/m6e5j/2VogY0+d5e2HQdB04t89/1O/w1cDnyilFU=')
-# Channel Secret
-handler = WebhookHandler('3c71b07a2d7861abf6a78d8845338baf')
+ACCESS_TOKEN = 'lIKFwU/TgvvYQAVMTT0V3L/Q4MJUnjZFb4TdtVb4xeb3YRAVMkRusqyAiIifWjdhFzwiWPT0u8F4B9iB1ILEjXqWEjNEy9xvClCqk8xnFjPVfl1MLFsJ/k6nam1Y94ksJNiLuuU0poOvR905pXJ8JwdB04t89/1O/w1cDnyilFU='
+SECRET = 'f470cac6f78984857d780a3b2a5e90fd'
 
-# 監聽所有來自 /callback 的 Post Request
+line_bot_api = LineBotApi(ACCESS_TOKEN)
+handler = WebhookHandler(SECRET)
+
 @app.route("/callback", methods=['POST'])
 def callback():
-    # get X-Line-Signature header value
     signature = request.headers['X-Line-Signature']
-    # get request body as text
     body = request.get_data(as_text=True)
     app.logger.info("Request body: " + body)
-    # handle webhook body
+    
     try:
         handler.handle(body, signature)
     except InvalidSignatureError:
         abort(400)
     return 'OK'
-
-# 處理訊息
+    
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    msg_from_user = event.message.text
-    if msg_from_user == 'Data-covid':
-    	message = TextSendMessage("Data COVID-19 " + negara + "\nPositif: " + positif + "\nSembuh: " + sembuh + "\nMeninggal: " + meninggal)
-    	line_bot_api.reply_message(event.reply_token, message)
+    line_bot_api.reply_message(
+        event.reply_token,
+        ImageSendMessage(
+            original_content_url='https://1.bp.blogspot.com/-eaDZ7sDP9uY/Xhwqlve5SUI/AAAAAAABXBo/EcI2C2vim7w2WV6EYy3ap0QLirX7RPohgCNcBGAsYHQ/s400/pose_syanikamaeru_man.png',
+            preview_image_url='https://1.bp.blogspot.com/-eaDZ7sDP9uY/Xhwqlve5SUI/AAAAAAABXBo/EcI2C2vim7w2WV6EYy3ap0QLirX7RPohgCNcBGAsYHQ/s400/pose_syanikamaeru_man.png'))
 
-import os
 if __name__ == "__main__":
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+    app.run()
